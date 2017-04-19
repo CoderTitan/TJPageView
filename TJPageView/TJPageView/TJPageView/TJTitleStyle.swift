b0e5e2a8b3bb4a60f539209e23412f4f6045d832
田全军 <tianquanjun@tianquanjundeMacBook-Pro.local>
2017-04-20 01:26:17 +0800

    完善功能

--- TJPageView/TJPageView/TJPageView/TJTitleStyle.swift
+++ TJPageView/TJPageView/TJPageView/TJTitleStyle.swift
@@ -17,5 +17,22 @@ class TJTitleStyle {
     var isScrollEnable : Bool = false// 是否可以滚动
     var itemMargin : CGFloat = 30           //间距
 
+    //**底部滚动条*/
+    var isShowBottomLine : Bool = true//
+    var bottomLineColor : UIColor = UIColor.red
+    var bottomLineHeight : CGFloat = 2.0
+    
+    //
+    var isTitleScale : Bool = false
+    var scaleRange : CGFloat = 1.2
+
+    //**表面滚动块*/
+    var isShowCoverView : Bool = true
+    var coverColor : UIColor = UIColor.black
+    var coverAlpha : CGFloat = 0.4
+    var coverMagin : CGFloat = 8
+    var coverHeight : CGFloat = 25
+    
+    
     
 }
--- TJPageView/TJPageView/TJPageView/TJTitleView.swift
+++ TJPageView/TJPageView/TJPageView/TJTitleView.swift
@@ -25,6 +25,18 @@ class TJTitleView: UIView {
         scrolView.scrollsToTop = false
         return scrolView
     }()
+    fileprivate lazy var bottomLine : UIView = {
+        let line = UIView()
+        line.backgroundColor = self.style.bottomLineColor
+        line.frame.size.height = self.style.bottomLineHeight
+        return line
+    }()
+    fileprivate lazy var coverView : UIView = {
+       let coverView = UIView()
+        coverView.backgroundColor = self.style.coverColor
+        coverView.alpha = self.style.coverAlpha
+        return coverView
+    }()
 
     init(frame: CGRect, titles : [String], style : TJTitleStyle) {
         self.titles = titles
@@ -35,7 +47,7 @@ class TJTitleView: UIView {
     }
 
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("不能从Xib中加载")
     }
 }
 
@@ -46,6 +58,31 @@ extension TJTitleView{
         addTitleLabels()
         //设置label的frame
         setupTitleLabelFrame()
+        
+        //设置滚动条和滚动块
+        addBottomLineAndCoverViewFrame()
+    }
+    private func addBottomLineAndCoverViewFrame(){
+        guard style.isShowBottomLine else {
+            return
+        }
+        scrollView.addSubview(bottomLine)
+        let lineFrame = CGRect(x: titleLabels.first!.frame.origin.x, y: bounds.height - style.bottomLineHeight, width: titleLabels.first!.frame.size.width, height: style.bottomLineHeight)
+        bottomLine.frame = lineFrame
+        
+        //滚动块
+        guard style.isShowCoverView else {
+            return
+        }
+        scrollView.addSubview(coverView)
+        var coverW : CGFloat = titleLabels.first!.frame.size.width - 2 * style.coverMagin
+        if style.isScrollEnable {
+            coverW = titleLabels.first!.frame.size.width + style.coverMagin * 0.5
+        }
+        coverView.bounds = CGRect(x: 0, y: 0, width: coverW, height: style.coverHeight)
+        coverView.center = titleLabels.first!.center
+        coverView.layer.masksToBounds = true
+        coverView.layer.cornerRadius = style.coverHeight * 0.5
     }
     private func addTitleLabels(){
         for (i ,title) in titles.enumerated() {
@@ -92,6 +129,23 @@ extension TJTitleView{
         adjustTitleLabel(targetIndex: targetLabel.tag)
         //设置代理
         delegate?.titleView(self, targetIndex: currentIndex)
+        
+        //调整bottomLine
+        if style.isShowBottomLine {
+            bottomLine.frame.origin.x = targetLabel.frame.origin.x
+            bottomLine.frame.size.width = targetLabel.frame.size.width
+        }
+        // 4.调整缩放比例
+        if style.isTitleScale {
+            targetLabel.transform = titleLabels[currentIndex].transform
+            titleLabels[currentIndex].transform = CGAffineTransform.identity
+        }
+        // 6.调整coverView的位置
+        if style.isShowCoverView {
+            let coverW = style.isScrollEnable ? targetLabel.frame.size.width + style.coverMagin : targetLabel.frame.size.width - 2 * style.coverMagin
+            coverView.frame.size.width = coverW
+            coverView.center = targetLabel.center
+        }
     }
     
     fileprivate func adjustTitleLabel(targetIndex : Int){
@@ -128,13 +182,36 @@ extension TJTitleView : TJContentViewDelegate{
         //取出label
         let targetLabel = titleLabels[targetIndex]
         let sourceLabel = titleLabels[currentIndex]
-        print(currentIndex,targetIndex)
+        
         //修改渐变颜色
         let deltaRGB = UIColor.getRGBDelta(style.selectColor, style.normalColor)
         let selectRGB = style.selectColor.getRGB()
         let normalRGB = style.normalColor.getRGB()
         targetLabel.textColor = UIColor(r: normalRGB.0 + deltaRGB.0 * progress, g: normalRGB.1 + deltaRGB.1 * progress, b: normalRGB.2 + deltaRGB.2 * progress)
         sourceLabel.textColor = UIColor(r: selectRGB.0 - deltaRGB.0 * progress, g: selectRGB.1 - deltaRGB.1 * progress, b: selectRGB.2 - deltaRGB.2 * progress)
-
+        
+        //调整渐变滑动条
+        if style.isShowBottomLine {
+            let deltaX = targetLabel.frame.origin.x - sourceLabel.frame.origin.x
+            let deltaW = targetLabel.frame.size.width - sourceLabel.frame.size.width
+            bottomLine.frame.origin.x = sourceLabel.frame.origin.x + deltaX * progress
+            bottomLine.frame.size.width = sourceLabel.frame.size.width + deltaW * progress
+        }
+        
+        //调节缩放
+        if style.isTitleScale {
+            let deltaScale = style.scaleRange - 1.0
+            sourceLabel.transform = CGAffineTransform(scaleX: style.scaleRange - deltaScale * progress, y: style.scaleRange - deltaScale * progress)
+            targetLabel.transform = CGAffineTransform(scaleX: 1.0 + deltaScale * progress, y: 1.0 + deltaScale * progress)
+        }
+        //调节渐变滑动块
+        if style.isShowCoverView {
+            let sourceLabelW = style.isScrollEnable ? (sourceLabel.frame.width + style.itemMargin) : (sourceLabel.frame.width - 2 * style.coverMagin)
+            let targetLabelW = style.isScrollEnable ? (targetLabel.frame.width + style.itemMargin) : (targetLabel.frame.width - 2 * style.coverMagin)
+            let deltaW = targetLabelW - sourceLabelW
+            let deltaX = targetLabel.center.x - sourceLabel.center.x
+            coverView.frame.size.width = sourceLabelW + deltaW * progress
+            coverView.center.x = sourceLabel.center.x + deltaX * progress
+        }
     }
 }
